0bf5908d14bbecaaa8c793d9ed418d2fb634880f
lwall <test@test.com>
2024-03-02 16:28:55 +0330

    removed some files and made some folders

---
+++ python-scripts/whatsapp_bot.py
@@ -0,0 +1,7 @@
+import pyautogui as auto
+import time
+
+while True:
+  auto.write("FUCK")
+  auto.press('enter')
+  
--- whatsapp_bot.py
+++
@@ -1,7 +0,0 @@
-import pyautogui as auto
-import time
-
-while True:
-  auto.write("bye bye")
-  auto.press('enter')
-  
