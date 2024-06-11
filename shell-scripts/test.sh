0bfc6d45b269e7c8266971f0567833f5ce976cc2
Your Name <you@example.com>
2024-06-11 17:15:22 +0330

    added shell scripts macro

---
+++ shell-scripts/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# i built the array using 'macro'
+#
+# what u do is u use 'q' + kayname=ex:h
+# then u start the stuff u do 
+# then when done press q again
+# and done
+
+array = [
+	"test",
+	"go",
+	"mybrother",
+	"fucker",
+	"jack",
+]
